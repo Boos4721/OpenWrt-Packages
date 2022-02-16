3f4d4f83666125c79cd3fad4df269764678c925c
boos4721 <3.1415926535boos@gmail.com>
2022-02-16 14:24:08 +0800

    [bot] AutoMerging: 20220216
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- luci-app-mqos/luasrc/controller/mqos.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.mqos", package.seeall)
-
-function index()
-	if not nixio.fs.access("/etc/config/mqos") then
-		return
-	end
-	
-	entry({"admin", "network", "mqos"}, cbi("mqos/mqos"), _("MQoS"))
-end
