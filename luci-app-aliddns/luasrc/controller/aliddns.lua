b132ca24f8aca444697488d6463f954c6fb6328a
boos4721 <3.1415926535boos@gmail.com>
2022-06-20 04:22:48 +0800

    [bot] AutoMerging: 20220620
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- luci-app-aliddns/luasrc/controller/aliddns.lua
+++ luci-app-aliddns/luasrc/controller/aliddns.lua
@@ -1,11 +1,4 @@
-module("luci.controller.aliddns", package.seeall)
-
+module("luci.controller.aliddns",package.seeall)
 function index()
-	if not nixio.fs.access("/etc/config/aliddns") then
-		return
-	end
-
-	local page = entry({"admin", "services", "aliddns"}, cbi("aliddns"), _("AliDDNS"), 58)
-	page.dependent = true
-	page.acl_depends = { "luci-app-aliddns" }
+entry({"admin","services","aliddns"},cbi("aliddns"),_("AliDDNS"),58).acl_depends = { "luci-app-aliddns" }
 end
